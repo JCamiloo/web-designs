752bd59b2a70962183d0efd25c7190c8a8e8c4ef
Juan Camilo <jcamilo.osorio15@gmail.com>
2019-10-26 22:23:44 -0500

    [uHost] adding navigation bar

--- [uHost] Web 1/shared.js
+++ [uHost] Web 1/shared.js
@@ -1,10 +1,19 @@
-let backdrop = document.querySelector('.backdrop');
-let modal = document.querySelector('.modal');
-let planButtons = document.querySelectorAll('.plan button');
+const backdrop = document.querySelector('.backdrop');
+const modal = document.querySelector('.modal');
+const planButtons = document.querySelectorAll('.plan button');
+const noButton = document.querySelector(".modal__action--negative");
 
 for(button of planButtons) {
     button.addEventListener('click', function() {
         modal.style.display = 'block';
         backdrop.style.display = 'block';
     });
+}
+
+backdrop.addEventListener("click", closeModal);
+noButton.addEventListener("click", closeModal);
+
+function closeModal() {
+    backdrop.style.display = "none";
+    modal.style.display = "none";
 }
